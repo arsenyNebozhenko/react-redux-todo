2c4e095cd3a45872d1a02ea91f2b89289f14cf12
arsenyNebozhenko <nebozhenko.arseny@gmail.com>
2020-01-31 20:19:54 +0300

    added placeholder state

--- src/reducers/todosReducer.js
+++ src/reducers/todosReducer.js
@@ -1,4 +1,20 @@
-const initialState = []
+const initialState = [
+  {
+    id: 1,
+    title: 'test 1',
+    status: 'active'
+  },
+  {
+    id: 2,
+    title: 'test 2',
+    status: 'completed'
+  },
+  {
+    id: 3,
+    title: 'test 3',
+    status: 'failed'
+  }
+]
 
 const todosReducer = (state = initialState, action) => {
   switch (action.type) {
