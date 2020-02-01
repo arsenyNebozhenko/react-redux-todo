3ac48462112d4acd562e86c1aa37ba7a18bff9da
arsenyNebozhenko <nebozhenko.arseny@gmail.com>
2020-02-02 00:57:19 +0300

    change item structure

--- src/components/TodoItem/TodoItem.js
+++ src/components/TodoItem/TodoItem.js
@@ -6,10 +6,10 @@ import {
 } from '../../utils'
 
 const TodoItem = ({ todo: { id, title, description, importance, dateAdded, dateExpires, dateCompleted, status, isHidden }, deleteItem,  setItemProp }) => {
-  const style = {
-    display: isHidden ? 'none' : 'block',
-    color: status === 'active' ? 'black' : status === 'completed' ? 'green' : 'red'
-  }
+  // const style = {
+  //   display: isHidden ? 'none' : 'block',
+  //   color: status === 'active' ? 'black' : status === 'completed' ? 'green' : 'red'
+  // }
   
   const handleToggleButtonClick = () => {
     if (status === 'completed') {
@@ -21,22 +21,24 @@ const TodoItem = ({ todo: { id, title, description, importance, dateAdded, dateE
   }
   
   return (
-    <li style={style}>
+    <li className="todo-item">
+      <button onClick={handleToggleButtonClick}>&#10004;</button>
       <input 
+        className="todo-item__input"
         type="text" 
         name="title" 
         value={title} 
-        onChange={({ target: { name, value }}) => setItemProp(name, value)} />
-      <div>Added: {getFormattedDate(dateAdded)}</div>
-      {isValidDate(dateExpires) && <div>Expires: {getFormattedDate(dateExpires)}</div>}
-      {dateCompleted && <div>Completed: {getFormattedDate(dateCompleted)}</div>}
+        onChange={({ target: { name, value }}) => setItemProp(name, value)} 
+      />
+      <strong>{status}</strong>
+      <ul className="todo-item__dates">
+        <li className="todo-item__date">Added: {getFormattedDate(dateAdded)}</li>
+        {isValidDate(dateExpires) && <li className="todo-item__date">Expires: {getFormattedDate(dateExpires)}</li>}
+        {dateCompleted && <li className="todo-item__date">Completed: {getFormattedDate(dateCompleted)}</li>}
+      </ul>
       {description && <div>{description}</div>}
       {importance && <div>{importance}</div>}
-      <strong>{status}</strong>
-      <div>
-        <button onClick={handleToggleButtonClick}>&#10004;</button>
-        <button onClick={() => deleteItem(id)}>&times;</button>
-      </div>
+      <button onClick={() => deleteItem(id)}>&times;</button>
     </li>
   )
 }
