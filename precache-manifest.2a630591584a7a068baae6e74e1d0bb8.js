6c3e5231648886884dad92d47e3e510b0bf266c3
arsenyNebozhenko <nebozhenko.arseny@gmail.com>
2020-02-03 01:48:09 +0300

    Updates

---
+++ precache-manifest.2a630591584a7a068baae6e74e1d0bb8.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9002b57697029eb0af1b5833a8fe608b",
+    "url": "/react-redux-todo/index.html"
+  },
+  {
+    "revision": "8faa0fa405221627b58e",
+    "url": "/react-redux-todo/static/css/main.566a7eb0.chunk.css"
+  },
+  {
+    "revision": "b3ee0b3d34769842108f",
+    "url": "/react-redux-todo/static/js/2.4238f524.chunk.js"
+  },
+  {
+    "revision": "928d7b5eb39d16fe9a880722c974b51e",
+    "url": "/react-redux-todo/static/js/2.4238f524.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8faa0fa405221627b58e",
+    "url": "/react-redux-todo/static/js/main.cf84cf14.chunk.js"
+  },
+  {
+    "revision": "7c7024619c201c9627e8",
+    "url": "/react-redux-todo/static/js/runtime-main.b42a28ab.js"
+  },
+  {
+    "revision": "7d9f866a4286276658629ffd9a3ecadf",
+    "url": "/react-redux-todo/static/media/gear.7d9f866a.svg"
+  }
+]);
