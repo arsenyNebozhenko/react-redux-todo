ccf4f4ae2da393a22bc1f33bfcedf4d153123235
arsenyNebozhenko <nebozhenko.arseny@gmail.com>
2020-02-03 00:34:45 +0300

    refactoring

--- src/reducers/todosReducer.js
+++ src/reducers/todosReducer.js
@@ -4,20 +4,23 @@ import { isValidDate } from '../utils'
 const initialState = JSON.parse(localStorage.getItem('todos'))
 
 const todosReducer = (state = initialState, action) => {
-  switch (action.type) {
+  const { type } = action
+  const { todo, id, key, value } = action.payload ? action.payload : action
+
+  switch (type) {
     case ADD_ITEM:
       return [
         ...state,
-        action.payload.todo
+        todo
       ]
     case DELETE_ITEM:
-      return state.filter(item => item.id !== action.payload.id)
+      return state.filter(item => item.id !== id)
     case DISPLAY_ITEMS_BY_IMPORTANCE:
-      return action.payload.value === 'all' ? state.map(item => ({...item, isHidden: false})) : state.map(item => item.importance === action.payload.value ? {...item, isHidden: false} : {...item, isHidden: true})
+      return value === 'all' ? state.map(item => ({...item, isHidden: false})) : state.map(item => item.importance === value ? {...item, isHidden: false} : {...item, isHidden: true})
     case DETECT_ITEMS_FAILURE:
       return state.map(item => item.status === 'active' && isValidDate(item.dateExpires) && new Date(item.dateExpires).getTime() <= new Date(item.dateAdded).getTime() ? {...item, status: 'failed'} : item)
     case SET_ITEM_PROP:
-      return state.map(item => item.id === action.payload.id ? {...item, [action.payload.key]: action.payload.value} : item)
+      return state.map(item => item.id === id ? {...item, [key]: value} : item)
     default:
       return state
   }
