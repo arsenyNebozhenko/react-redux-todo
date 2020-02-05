ffa269a907d6252e733844a5b4d7397cc8b27161
arsenyNebozhenko <nebozhenko.arseny@gmail.com>
2020-02-05 20:42:04 +0300

    rework of Todo

--- src/components/Todo/Todo.js
+++ src/components/Todo/Todo.js
@@ -4,14 +4,25 @@ import './Todo.scss'
 import TodoAddForm from '../TodoAddForm/TodoAddForm'
 import TodoList from '../TodoList/TodoList'
 import TodoFilterForm from '../TodoFilterForm/TodoFilterForm'
+import styled from 'styled-components'
+
+const Container = styled.div`
+  max-width: 1028px;
+  width: 100%;
+  margin: auto;
+  padding: 1rem;
+  background-color: #fff;
+  border-radius: 3px;
+  box-shadow: 0 0 16px .3rem #D1D5DA;
+`
 
 const Todo = () => {
   return (
-    <div className="todo">
+    <Container>
       <TodoAddForm />
       <TodoList />
       <TodoFilterForm />
-    </div>
+    </Container>
   )
 }
 
