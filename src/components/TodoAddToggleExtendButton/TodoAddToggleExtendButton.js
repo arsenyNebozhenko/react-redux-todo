6ae875f91e8766d588a1729ddc8cc0cd91d58c17
arsenyNebozhenko <nebozhenko.arseny@gmail.com>
2020-02-05 20:57:26 +0300

    rework of TodoAddToggleExtendButton

--- src/components/TodoAddToggleExtendButton/TodoAddToggleExtendButton.js
+++ src/components/TodoAddToggleExtendButton/TodoAddToggleExtendButton.js
@@ -1,15 +1,40 @@
 import React from 'react'
-import './TodoAddToggleExtendButton.scss'
+import styled from 'styled-components'
 
-import { ReactComponent as Gear } from '../../assets/gear.svg'
+import { ReactComponent as Gear} from '../../assets/gear.svg'
+
+const Button = styled.button`
+  flex-shrink: 0;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  width: 4rem;
+  height: 4rem;
+  background: transparent;
+  border: 0;
+  margin-right: .16rem;
+  cursor: pointer;
+  &:hover,
+  &:focus {
+    svg {
+      fill: #5897FE;
+    }
+  }
+`
+
+const Icon = styled(Gear)`
+  width: 3rem;
+  height: 3rem;
+  fill: #323639;
+  transition: all .2s ease;
+`
 
 const TodoAddToggleExtendButton = ({ toggleExtendForm }) => {
   return (
-    <button 
-      className="todo-add-toggle-extend-button"
+    <Button 
       type="button"
       onClick={toggleExtendForm}
-    ><Gear /></button>
+    ><Icon /></Button>
   )
 }
 
